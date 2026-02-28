ade7c3b4b0f54b6514f54fe6b45a3a99ba4680b1
Tri-Adi-Putra <triadiputra.js.com>
2026-02-28 19:49:31 +0700

    Membuat module say hello

---
+++ say-hello.go
@@ -0,0 +1,7 @@
+package go_say_hello
+
+// Perhatikan huruf besar 'S' pada SayHello
+// Di Go-Lang, huruf besar di awal berarti PUBLIC (bisa diakses dari luar)
+func SayHello() string {
+	return "Hello"
+}
